7ac2e5d32408abfe47b8bd713ad4737d05c114cc
raehyun <sakill07@naver.com>
2017-12-06 02:53:42 +0900

    First

---
+++ app.js
@@ -0,0 +1,68 @@
+var express = require('express'); // app server
+var bodyParser = require('body-parser'); // parser for post requests
+var Conversation = require('watson-developer-cloud/conversation/v1'); // watson sdk
+var app = express();
+
+// Bootstrap application settings
+app.use(express.static('./public')); // load UI from public folder
+app.use(bodyParser.json());
+
+// Create the service wrapper
+var conversation = new Conversation({
+  username: '2686b27a-aca5-4e06-8411-555d72a407c7', // replace with username from service key
+  password: 'ekTNWlv631DD', // replace with password from service key
+  version_date: '2017-05-26'
+});
+
+// Endpoint to be call from the client side
+app.post('/api/message', function(req, res) {
+  var workspace = 'f69368fd-38b4-454c-959d-626e6019696e';
+  
+  var payload = {
+    workspace_id: workspace,
+    context: req.body.context || {},
+    input: req.body.input || {}
+  };
+
+  // Send the input to the conversation service
+  conversation.message(payload, function(err, data) {
+    if (err) {
+      return res.status(err.code || 500).json(err);
+    }
+    return res.json(updateMessage(payload, data));
+  });
+});
+
+/**
+ * Updates the response text using the intent confidence
+ * @param  {Object} input The request to the Conversation service
+ * @param  {Object} response The response from the Conversation service
+ * @return {Object}          The response with the updated message
+ */
+function updateMessage(input, response) {
+  var responseText = null;
+  if (!response.output) {
+    response.output = {};
+  } else {
+    return response;
+  }
+  if (response.intents && response.intents[0]) {
+    var intent = response.intents[0];
+    // Depending on the confidence of the response the app can return different messages.
+    // The confidence will vary depending on how well the system is trained. The service will always try to assign
+    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
+    // user's intent . In these cases it is usually best to return a disambiguation message
+    // ('I did not understand your intent, please rephrase your question', etc..)
+    if (intent.confidence >= 0.75) {
+      responseText = 'I understood your intent was ' + intent.intent;
+    } else if (intent.confidence >= 0.5) {
+      responseText = 'I think your intent was ' + intent.intent;
+    } else {
+      responseText = 'I did not understand your intent';
+    }
+  }
+  response.output.text = responseText;
+  return response;
+}
+
+module.exports = app;
